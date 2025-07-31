bb1fa9ba166365e34529a7c1a28603166f6e91ca
Sanyam Singhal <sanyamsinghaliit@gmail.com>
2025-07-31 11:56:52 +0530

    Started adding the custom method interception logic for the OpenAI sync client. Captures kwargs + result for pre-parsed completion but currently does not capture result in the streaming mode, needs more plumbing

--- depths/logger/llm.py
+++ depths/logger/llm.py
@@ -1,6 +1,54 @@
-from openai import OpenAI
 from depths.logger.core import DepthsLogger
-from typing import Optional
+
+from openai import OpenAI
+
+from typing import Optional, Callable, Dict, Tuple, Any
+import functools
+
+def recursive_getattr(obj, attr, *args):
+    '''
+    Util function to recursively get attributes from an object.
+    '''
+    def _get(o, a): return getattr(o, a, *args)
+    return functools.reduce(_get, [obj] + attr.split('.'))
+
+def recursive_setattr(obj,attr,value):
+    '''
+    Util function to recursively set attributes on an object.
+    '''
+    pre, _, post = attr.rpartition('.')
+    target = recursive_getattr(obj, pre) if pre else obj
+    setattr(target, post, value)
+
+def make_wrapper(original, handler, path):
+    '''
+    Wrapper factory to define custom logging methods for specific
+    LLM client methods.
+
+    Agnostic of LLM client.
+    '''
+    def wrapped(*args, **kwargs):
+        result=original(*args, **kwargs)
+        handler(path, args, kwargs, result)
+        return result
+    return wrapped
+    
+def openai_handle_chat_create(
+    path:str,
+    args: Tuple[Any],
+    kwargs: Dict[str, Any],
+    result: Any
+    )->None:
+    '''
+    Custom logging handler for `OpenAI()` `chat.completions.create` method
+    '''
+    # To be implemented
+    print(args)
+    print(kwargs)
+
+OPENAI_METHOD_REGISTRY: Dict[str, Callable] = {
+    "chat.completions.create": openai_handle_chat_create
+}
 
 class LoggedOpenAI:
     '''
@@ -9,6 +57,11 @@ class LoggedOpenAI:
     def __init__(self,*args,logger: Optional[DepthsLogger] = None, **kwargs):
         self.client=OpenAI(*args, **kwargs)
         self.logger=logger
+
+        for path, handler in OPENAI_METHOD_REGISTRY.items():
+            original_func=recursive_getattr(self.client, path)
+            wrapped_func=make_wrapper(original_func, handler, path)
+            recursive_setattr(self.client, path, wrapped_func)
     
     def __getattr__(self, name: str):
         attr=getattr(self.client, name)
--- tests/test_llm_logging.py
+++ tests/test_llm_logging.py
@@ -3,13 +3,15 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
+MODEL="gemini-2.5-flash-lite"
+
 client=LoggedOpenAI(
     api_key=os.getenv("GEMINI_API_KEY"),
     base_url="https://generativelanguage.googleapis.com/v1beta/openai/"
     )
 
 response = client.chat.completions.create(
-    model="gemini-2.5-flash-lite",
+    model=MODEL,
     messages=[
         {"role": "system", "content": "You are a helpful assistant."},
         {
@@ -20,3 +22,18 @@ response = client.chat.completions.create(
 )
 
 print(response.choices[0].message.content[:20])
+
+stream = client.chat.completions.create(
+    model=MODEL,
+    messages=[
+        {
+            "role": "user",
+            "content": "Say 'Depths AI' ten times",
+        },
+    ],
+    stream=True,
+)
+
+for chunk in stream:
+    if chunk.choices[0].delta.content is not None:
+        print(chunk.choices[0].delta.content, end="")
